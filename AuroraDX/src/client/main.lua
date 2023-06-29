8f419941e51322d154962c6530b52109e6f9f950
foxy <69487888+foxyyx@users.noreply.github.com>
2023-06-28 21:00:16 -0300

    Update main.lua

--- AuroraDX/src/client/main.lua
+++ AuroraDX/src/client/main.lua
@@ -1,7 +1,2 @@
 
--- local button = import 'Button UI'
--- local chechbox = import 'CheckBox UI'
-
--- function createButton (...)
---     return button:create (...)
--- end
+screenW, screenH = guiGetScreenSize()
